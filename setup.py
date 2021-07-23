47932a7ba90f855cbb959c7e3fa15e380a795cc0
CDN-BC <fursham.h@gmail.com>
2021-07-23 11:00:54 +0100

    Removed unnecessary dependencies

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ VERSION = '0.1.0'
 
 # What packages are required for this module to be executed?
 REQUIRED = [
-    'pandas', 'matplotlib', 'sys', 'getopt', 'math', 'numpy', 'seaborn','os'
+    'pandas', 'matplotlib', 'numpy', 'seaborn'
 ]
 
 # What packages are optional?
